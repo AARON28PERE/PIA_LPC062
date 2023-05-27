41d9f6c9d9105f86aed1c7a6087dc7b70a50e22a
AARON28PERE <134762292+AARON28PERE@users.noreply.github.com>
2023-05-27 01:33:01 -0600

    Create scan_portv 4.py

---
+++ Escanér de puertos/scan_portv 4.py
@@ -0,0 +1,17 @@
+#Nombre: Aaron Perez Esparza 
+#Matricula: 1993809
+
+import nmap
+
+
+scanner = nmap. PortScanner ()
+scanner.scan ('192.168.100.228','1-1024','-v-sV'
+scanner. command_line()
+scanner.all hosts()
+scanner ['192.168.100.228' ].state()
+scanner ['192.168.100.228'].all_protocols()
+scanner ['192.168.100.228' ]['tcp'].keys()
+scanner ['192.168.100.228'].has_tcp(21)
+scanner ['192.168.100.228 ].has_tcp (22)
+scanner ['192.168.100.228']['tcp'1[22]
+scanner ['192.168.100.228 ]['tcp'][22][product ]
