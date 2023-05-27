3840cfd014de70fcf3b70fadf9e1e0922ad78d13
AARON28PERE <134762292+AARON28PERE@users.noreply.github.com>
2023-05-27 00:57:03 -0600

    Update number.sh

--- Scripting en Bash/number.sh
+++ Scripting en Bash/number.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+# Script welcome.sh
+# 04/03/2023 -Aaron Perez Espaza
+# 
+#Leer 3 numeros asisgnados a 3 variables
+read -p "Proporciona un numero para variable 1: " n1
+read -p "Proporciona un numero para variable 2: " n2
+read -p "Proporciona un numero para variable 3: " n3
+#
+#Despliega los 3 numeros proporcionados por el usuario
+echo "Numero 1 - $n1"
+echo "Numero 2 - $n2"
+echo "Numero 3 - $n3"
