a32009f6edbd3b85a9994f3aeb3dbb498c2ab07d
AARON28PERE <134762292+AARON28PERE@users.noreply.github.com>
2023-05-27 02:03:00 -0600

    Update scan_portv1.ps1

--- Scripting en PowerShell/scan_portv1.ps1
+++ Scripting en PowerShell/scan_portv1.ps1
@@ -1,3 +1,4 @@
+# Aaron Perez Esparza 1993809
 $portstoscan = @(20,21,22,23,25,50,51,53,80,110,119,135,136,137,138,139,143,161,162,389,443,445,636,1025,1443,3389,5985,5985,5986,8080,10000)
 $waittime = 100
 Write-Host "Direccion ip a escanear: " -NoNewline
